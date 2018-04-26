8101c1e4c8462c32043b2a7cc163c46f5dcac72f
Jayleh <jaylehyun@gmail.com>
2018-04-25 22:37:38 -0700

    added progress bar, loads when scrape button clicked

--- mars_mission/static/index.js
+++ mars_mission/static/index.js
@@ -1,10 +1,41 @@
-// Reference scrape button by its id
-var $scrapeBtn = document.getElementById("scrape-btn");
+// // Reference scrape button by its id
+// var $scrapeBtn = document.getElementById("scrape-btn");
 
-// Trigger event handler when scrape button is clicked
-function handleScrapeClick() {
-    $scrapeBtn.setAttribute("disabled", "disabled");
+// // Trigger event handler when scrape button is clicked
+// function handleScrapeClick() {
+//     $scrapeBtn.setAttribute("disabled", "disabled");
+// }
+
+// // Add event listiner to scrape button
+// $scrapeBtn.addEventListener("click", handleScrapeClick);
+
+
+// Function to show progress bar
+function showProgress() {
+    var elem = d3.select(".progress-bar");
+    var width = 0;
+    var id = setInterval(frame, 550);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style("width", `${width}%`);
+        }
+    }
 }
 
-// Add event listiner to scrape button
-$scrapeBtn.addEventListener("click", handleScrapeClick);
+
+var $scrapeBtn = d3.select("#scrape-btn");
+
+$scrapeBtn.on("click", function () {
+    // Disable button
+    $scrapeBtn.attr("disabled", "disabled");
+
+    // Make progress bar visible
+    var $progressBar = d3.select(".progress");
+    $progressBar.style("visibility", "visible");
+
+    showProgress();
+
+})
