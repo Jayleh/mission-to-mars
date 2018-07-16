9287769b65343788ae00431a2fc0306d926ca225
Jayleh <jaylehyun@gmail.com>
2018-07-16 11:23:20 -0700

    add in variables

--- mars_mission/__init__.py
+++ mars_mission/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-# app.config['MONGO_DBNAME'] = os.environ['MONGO_DBNAME']
-app.config['MONGO_URI'] = "mongodb://localhost:27017/app"
-# app.config['MONGO_URI'] = os.environ['MONGODB_URI']
+app.config['MONGO_DBNAME'] = os.environ['MONGO_DBNAME']
+# app.config['MONGO_URI'] = "mongodb://localhost:27017/app"
+app.config['MONGO_URI'] = os.environ['MONGODB_URI']
 
 mongo = PyMongo(app)
 
